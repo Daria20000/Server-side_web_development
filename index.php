fbbf31a3a72a0758e2bbca162b2c8ea561a6d1d4
Darya <du404132@gmail.com>
2026-05-25 02:04:53 +0500

    Done lab2.1

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+function myAutoLoader(string $className)
+{
+    require_once __DIR__ . '/src/' . str_replace('\\', '/', $className) . '.php';
+}
+spl_autoload_register('myAutoLoader');
+
+$route = ltrim($_SERVER['REQUEST_URI'], '/');
+$routes = require __DIR__ . '/src/routes/routes.php';
+
+$isRouteFound = false;
+
+foreach ($routes as $pattern => $controllerAndAction) {
+    preg_match($pattern, $route, $matches);
+    if (!empty($matches)) {
+        $isRouteFound = true;
+        break;
+    }
+}
+
+if (!$isRouteFound) {
+    echo 'Страница не найдена!';
+    return;
+}
+
+unset($matches[0]);
+
+$controllerName = $controllerAndAction[0];
+$actionName = $controllerAndAction[1];
+
+$controller = new $controllerName();
+$controller->$actionName(...$matches);
